9158f6318c5551b2fa4bac484d226aaf7a628898
Noteee <lugosi.norbert@gmail.com>
2016-10-26 15:25:16 +0200

    végső calculator

--- calculator2.py
+++ calculator2.py
@@ -1,17 +1,18 @@
 while True:
     try:
         num_a = float(input("Enter a number (or a letter to exit):   "))
-        #Bekéri az első számot és float-á alakítja ha tudja
+                                                                #Bekéri az első számot és float-á alakítja ha tudja
         operator = input("Enter an operation!:   "   )
-        #Bekéri az operátort
+                                                                #Bekéri az operátort
         num_b = float(input("Enter another number:   "))
-        #Bekéri a második számot és floattá alakítja ha tudja
+                                                                #Bekéri a második számot és floattá alakítja ha tudja
     except:
         break
-        #Mivel a bekérés számot vár így ha nem azt kap azt hibának veszi ilyenkor kilép a ciklusból
+                                                                #Mivel csak a számokat tudja floattá alakítani, 
+                                                                #ha nem szám akkor hibának veszi ilyenkor kilép a ciklusból
     
     if (operator != "+") | (operator != "-") | (operator != "/") | (operator != "*"):
-        
+                                                                #csak akkor fussunk bele ha megfelelő az operátor
             if operator == "+":
                 print("%s:  %.2f" % ("Result", num_a + num_b))
 
@@ -23,11 +24,12 @@ while True:
 
             elif operator == "*":
                 print("%s:  %.2f" % ("Result", num_a * num_b))
+                                                                #Különböző esetek lekezelése 
 
-    
     print('Operators can be: + or - or / or *')
-        
+                                                                #megfelelő operátorok kiíratása
                 
-            #Különböző esetek lekezelése ^
+            
 
 print('Leaving Calculator')
+                                                                #kilépés kiíratása
