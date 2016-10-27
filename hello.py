1633786e349c13633642a3e706a53bdc258470ed
Noteee <lugosi.norbert@gmail.com>
2016-10-27 14:52:25 +0200

    végleges

--- hello.py
+++ hello.py
@@ -1,23 +1,19 @@
 import sys
 
+
 # This function determines the number of cmdl agruments and returns these arguments as
 #a title type string, if theres no arguments other than the file name it returns "World" 
 def argname ():
-    if len(sys.argv)==1:
+    lenght = len(sys.argv)
+    if lenght == 1:
         return "World"
-    elif len(sys.argv) ==2:
-        return sys.argv[(len(sys.argv)-1)].title()
-    elif len(sys.argv) ==3:
-        return sys.argv[(len(sys.argv)-2)].title() + " " + sys.argv[(len(sys.argv)-1)].title()
+    elif lenght == 2:
+        return sys.argv[(lenght-1)].title()
+    elif lenght == 3:
+        return sys.argv[(lenght-2)].title() + " " + sys.argv[(lenght-1)].title()
     else:
         return "World"
     
+
 #prints out the "Hello" string and the argname-return-value and a "!""
 print("%s %s%s " % ("Hello", argname(), "!"))
-
-
-
-
-
-
-
